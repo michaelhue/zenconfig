d49b1e37d64f8be635de69a022bb027f4cd1e572
Michael Hüneburg <hello@michaelhue.com>
2016-07-21 21:07:23 +0200

    Move factory code to own module
    Preparation for an improved testing setup. This removes all logic from
    the package's main index.js file, which is now just a proxy for
    lib/factory.js.

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-const config = require('./lib/config')
-const errors = require('./lib/errors')
-const { types } = require('./lib/schema')
-
 /**
- * Public factory function.
- * @param {...*} args
- * @return {Object}
- */
-function factory(...args) {
-  return config(...args)
-}
-
-// Attach errors/types.
-Object.assign(factory, errors, types)
-
-/**
- * Zenconfig module.
+ * zenconfig module
  * @module zenconfig
  */
-module.exports = factory
+module.exports = require('./lib/factory')
